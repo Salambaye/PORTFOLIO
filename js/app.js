32346adf939384c9279cbf28a7d0232c82706957
Sala <Salambaye2001@gmail.com>
2024-06-28 14:30:38 +0200

    Améliorations du code

--- js/app.js
+++ js/app.js
@@ -1,25 +1,16 @@
 const openMenu = () => {
     const menu = document.querySelector(".header-menu");
+    //console.log(menu)
 
-    // menu.style.display = "flex"
-    // console.log()
+    //menu.style.display = "flex"
 
     menu.classList.toggle("active");   //Toogle pour pouvoir effacer le menu quand on veut sortir
-    console.log("Menu toggled");
+    
+    if (menu.classList.contains("active")) {
+        document.querySelector("header .material-symbols-outlined").innerHTML = "close"
+    } 
+    else {
+        document.querySelector("header .material-symbols-outlined").innerHTML = "menu"    
+    }
 
-    // document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    //     anchor.addEventListener('click', function (e) {
-    //         e.preventDefault();
-
-    //         const target = document.querySelector(this.getAttribute('href'));
-    //         const headerOffset = document.querySelector('header').offsetHeight;
-    //         const elementPosition = target.offsetTop;
-    //         const offsetPosition = elementPosition - headerOffset;
-
-    //         window.scrollTo({
-    //             top: offsetPosition,
-    //             behavior: 'smooth'
-    //         });
-    //     });
-    // });
 }
